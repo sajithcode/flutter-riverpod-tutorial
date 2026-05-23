9533669c00cbcf6ade7f96e73e0e965d5b2192a4
Sajith Rathna Sampath <sajithsampath721@gmail.com>
2026-05-23 21:30:31 +0530

    feat: implement Riverpod state management and product listing UI with code generation

---
+++ lib/providers/cart_provider.dart
@@ -0,0 +1,16 @@
+
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_files/models/product.dart';
+
+class CartNotifire extends Notifier<Set<Product>>{
+  //intial value
+  @override
+  Set<Product> build(){
+    return {
+      Product(id: )
+    };
+    }
+
+  //methods to Update state
+}
